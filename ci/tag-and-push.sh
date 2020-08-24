73eb22361fbb3443c64c17c8ad042ea82b731066
AJ Slater <aj@slater.net>
2020-08-23 19:02:53 -0700

    fix varible

--- ci/tag-and-push.sh
+++ ci/tag-and-push.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 source ./env
-VERSION=${ALPINE_VERSION}_${PKG_VERSION}
+VERSION=${BASE_VERSION}_${PKG_VERSION}
 TAG=$VERSION-$(echo "$CIRCLE_SHA1" | head -c 7)
-docker tag $IMAGE "$IMAGE:$TAG"
-docker tag $IMAGE $IMAGE:latest
+docker tag "$IMAGE" "$IMAGE:$TAG"
+docker tag "$IMAGE" "$IMAGE:latest"
 docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
-docker push $REPO
+docker push "$REPO"
