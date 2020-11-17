a3580e39365eabac9f3b0bc1f8644a68a9ae04b3
AJ Slater <aj@slater.net>
2020-11-16 23:16:43 -0800

    Multi arch ci (#8)
    * multi-arch hooo
    
    * add platforms

--- ci/tag-and-push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-source ./env
-VERSION=python${BASE_VERSION}_${PKG_VERSION}
-TAG=$VERSION-$(echo "$CIRCLE_SHA1" | head -c 7)
-docker tag "$IMAGE" "$IMAGE:$TAG"
-docker tag "$IMAGE" "$IMAGE:latest"
-docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
-docker push "$REPO"
