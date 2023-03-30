568741555563ffbe4dbf148decdee1d7fcfeb41c
AJ Slater <aj@slater.net>
2023-03-30 13:29:03 -0700

    exec when using localuser. shellharden

--- build-multiarch.sh
+++ build-multiarch.sh
@@ -8,9 +8,9 @@ docker buildx create --use
 # shellcheck disable=SC2086
 docker buildx build \
     --platform "$PLATFORMS" \
-    --build-arg BASE_VERSION=$BASE_VERSION \
-    --build-arg VERSION=$VERSION \
+    --build-arg BASE_VERSION="$BASE_VERSION" \
+    --build-arg VERSION="$VERSION" \
     --tag "$REPO:${VERSION}" \
     --tag "$REPO:latest" \
-    ${PUSH:-} \
+    "${PUSH:-}" \
     .
