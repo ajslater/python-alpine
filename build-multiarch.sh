fc6521756cda12e98299fb4361e0c5a1b71929bf
AJ Slater <aj@slater.net>
2023-03-30 13:38:00 -0700

    use arrays to help shellharden

--- build-multiarch.sh
+++ build-multiarch.sh
@@ -5,12 +5,19 @@ source .env
 export DOCKER_CLI_EXPERIMENTAL=enabled
 export DOCKER_BUILDKIT=1
 docker buildx create --use
+
+BUILD_ARGS=(
+    --platform "$PLATFORMS"
+    --build-arg BASE_VERSION="$BASE_VERSION"
+    --build-arg VERSION="$VERSION"
+    --tag "$REPO:${VERSION}"
+    --tag "$REPO:latest"
+)
+if [ "${1-}" == 'push' ]; then
+    BUILD_ARGS+=(--push)
+fi
+
 # shellcheck disable=SC2086
 docker buildx build \
-    --platform "$PLATFORMS" \
-    --build-arg BASE_VERSION="$BASE_VERSION" \
-    --build-arg VERSION="$VERSION" \
-    --tag "$REPO:${VERSION}" \
-    --tag "$REPO:latest" \
-    "${PUSH:-}" \
-    .
+    "${BUILD_ARGS[@]-}"
+.
